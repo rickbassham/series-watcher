8ff51d20f4919024967328ddb3c1d96a431655a6
brodrick.bassham <brodrick.bassham@176e6e08-5052-d1e0-6508-233c934c0a6d>
2011-02-13 20:33:43 +0000

    Initial commit.

---
+++ trunk/src/series-watcher/ITVProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SeriesWatcher
+{
+    public interface ITVProvider
+    {
+        void Update();
+        Episode FindEpisode(int seriesId, int season, int episode);
+        Episode FindEpisode(int seriesId, DateTime dateTime);
+        Episode FindEpisode(string seriesName, int season, int episode);
+        Episode FindEpisode(string seriesName, DateTime dateTime);
+        Series GetFullSeries(int seriesId);
+    }
+}
